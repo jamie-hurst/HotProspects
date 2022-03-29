c87992932fb411d622348f259fa17d1ca1f0fb90
warmrome <pxpx786@gmail.com>
2022-03-29 14:00:53 -0700

    initial commit

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -2,15 +2,35 @@
 //  ContentView.swift
 //  HotProspects
 //
-//  Created by Jameson Hurst on 2/18/22.
+//  Created by Jameson Hurst on 2/25/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var prospects = Prospects()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+        .environmentObject(prospects)
     }
 }
 
