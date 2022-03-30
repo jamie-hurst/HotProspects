8f479fbacbd57387db2440a252afdffd3ec46691
warmrome <pxpx786@gmail.com>
2022-03-30 13:57:18 -0700

    Added a confirmation dialog that allows for custom sorting of ProspectsView.

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -15,9 +15,15 @@ struct ProspectsView: View {
         case none, contacted, uncontacted
     }
     
+    enum SortType {
+        case name, date
+    }
+    
     @EnvironmentObject var prospects: Prospects
     @State private var isShowingScanner = false
     
+    @State private var sortOrder = SortType.date
+    @State private var isShowingSortOptions = false
     
     let filter: FilterType
     
@@ -66,11 +72,25 @@ struct ProspectsView: View {
             }
             .navigationTitle(title)
             .toolbar {
-                Button {
-                    isShowingScanner = true
-                } label: {
-                    Label("Scan", systemImage: "qrcode.viewfinder")
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        isShowingSortOptions = true
+                    } label: {
+                        Label("Sort", systemImage: "arrow.up.arrow.down")
+                    }
                 }
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isShowingScanner = true
+                    } label: {
+                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    }
+                }
+            }
+            .confirmationDialog("Sort by…", isPresented: $isShowingSortOptions) {
+                Button("Name (A-Z)") { sortOrder = .name }
+                Button("Date (Newest first)") { sortOrder = .date }
             }
             .sheet(isPresented: $isShowingScanner) {
                 CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
@@ -91,13 +111,21 @@ struct ProspectsView: View {
     
     //filters the list data so one view can be transformed
     var filteredProspects: [Prospect] {
+        let result: [Prospect]
+        //filtering first, then stashing the result
         switch filter {
         case .none:
-            return prospects.people
+            result = prospects.people
         case .contacted:
-            return prospects.people.filter { $0.isContacted }
+            result = prospects.people.filter { $0.isContacted }
         case .uncontacted:
-            return prospects.people.filter { !$0.isContacted }
+            result = prospects.people.filter { !$0.isContacted }
+        }
+        //sorting based on previously filtered data
+        if sortOrder == .name {
+            return result.sorted { $0.name < $1.name }
+        } else {
+            return result.reversed()
         }
     }
     
